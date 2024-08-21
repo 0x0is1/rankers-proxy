c77c39b2ad89d37fd48a32b8850fea6802aa4437
Suryaa <0x0is1@protonmail.com>
2024-08-22 01:38:17 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import flask, requests
+app = Flask(__name__)
+
+BASE_URL = "https://api.ranker.com/"
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def forwarding(path):
+    url = BASE_URL + str(request.url).split('/')[1]
+    print(url)
+    response = requests.get(url, stream=True).json()
+    response = flask.jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
